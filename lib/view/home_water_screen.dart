17d5aae67029bbf1ac27bbd670d9ac26b871554f
MateusHBR <mateusmorais.r@gmail.com>
2020-06-15 14:03:28 -0300

    envolvendo com um scrollview

--- lib/view/home_water_screen.dart
+++ lib/view/home_water_screen.dart
@@ -22,105 +22,107 @@ class HomeWaterScreen extends StatelessWidget {
     return SingleChildScrollView(
       child: Container(
         height: size.height * 0.88,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                WaterSphere(),
-                Positioned(
-                  bottom: 0,
-                  right: 65,
-                  left: 65,
-                  child: InkWell(
-                    onTap: () {
-                      homeWaterStore.addValue();
-                      homeWaterStore.addCup(
-                        Cups(
-                          ml: 200,
-                          date: DateTime.now(),
-                        ),
-                      );
-                    },
-                    child: Stack(
-                      children: <Widget>[
-                        Opacity(
-                          opacity: 0.1,
-                          child: Container(
-                            height: 40,
-                            decoration: BoxDecoration(
-                              color: Colors.blue,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          child: Container(
-                            height: 40,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(Icons.local_drink),
-                                Text(
-                                  '+200',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.only(
-                    top: size.height * 0.04,
-                    left: size.width * 0.051,
-                  ),
-                  child: Text(
-                    'Registros de hoje',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(
-                horizontal: size.width * 0.04,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: size.height * 0.02),
+                child: WaterSphere(),
               ),
-              height: size.height * 0.4,
-              child: Card(
-                elevation: 6,
-                child: Observer(
-                  builder: (_) {
-                    return ListView.separated(
-                      separatorBuilder: (context, index) => Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: size.height * 0.02,
-                        ),
-                        child: Divider(),
+              Container(
+                padding: EdgeInsets.only(top: size.height * 0.02),
+                width: size.width * 0.4,
+                child: InkWell(
+                  onTap: () {
+                    homeWaterStore.addValue();
+                    homeWaterStore.addCup(
+                      Cups(
+                        ml: 200,
+                        date: DateTime.now(),
                       ),
-                      itemCount: homeWaterStore.cups.length,
-                      itemBuilder: (context, index) {
-                        return ItemListTile(
-                          ml: homeWaterStore.cups[index].ml.toString(),
-                          date: formatDate(homeWaterStore.cups[index].date),
-                        );
-                      },
                     );
                   },
+                  child: Stack(
+                    children: <Widget>[
+                      Opacity(
+                        opacity: 0.1,
+                        child: Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        child: Container(
+                          height: 40,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(Icons.local_drink),
+                              Text(
+                                '+200',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(
+                      top: size.height * 0.04,
+                      left: size.width * 0.051,
+                    ),
+                    child: Text(
+                      'Registros de hoje',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: size.width * 0.04,
+                ),
+                height: size.height * 0.35,
+                child: Card(
+                  elevation: 6,
+                  child: Observer(
+                    builder: (_) {
+                      return ListView.separated(
+                        separatorBuilder: (context, index) => Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: size.height * 0.02,
+                          ),
+                          child: Divider(),
+                        ),
+                        itemCount: homeWaterStore.cups.length,
+                        itemBuilder: (context, index) {
+                          var cup = homeWaterStore.cups[index];
+                          return ItemListTile(
+                            ml: cup.ml.toString(),
+                            date: formatDate(cup.date),
+                            function: () => homeWaterStore.removCup(index),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
